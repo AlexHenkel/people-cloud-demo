e1a1a880d1611e19120fce92726e7768489b695b
Alejandro Henkel <alehenkel17@gmail.com>
2015-08-25 10:44:03 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>People Cloud</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="stylesheets/styles.css">
+	
+	<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid navbar-container">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#"><i class="glyphicon glyphicon-cloud"></i></a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="navbar-item active"><a href="#">Datos del recurso<span class="sr-only">(current)</span></a></li>
+	        <li class="navbar-item"><a href="#">Planeación y Organización</a></li>
+	        <li class="navbar-item"><a href="#">Atracción</a></li>
+	        <li class="navbar-item"><a href="#">Administración</a></li>
+	        <li class="navbar-item"><a href="#">Retención</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</body>
+</html>
